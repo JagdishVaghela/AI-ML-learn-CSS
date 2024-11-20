00a804ceedff564e83417be365c1890863bdf716
JagdishVaghela <vjagdishp4@gmail.com>
2024-11-20 21:00:42 +0000

    Visualizing the data using numpy

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+import matplotlib.pyplot as plt 
+
 
 # Loading the dataset
 path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
@@ -11,3 +13,24 @@ print(df.head())
 print(df.describe()) 
 
 cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']] 
+
+# Visualizing the data
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']] 
+viz.hist() 
+
+#matplotlib library 
+plt.show() 
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue') 
+plt.xlabel("FUELCONSUMPTION_COMB") 
+plt.ylabel("Emission") 
+plt.show() 
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color='blue')
+plt.xlabel("Cylinders")
+plt.ylabel("Emission")
+plt.show()
