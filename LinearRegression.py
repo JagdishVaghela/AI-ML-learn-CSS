215d31419d4601a7f21839db942849837b6db858
JagdishVaghela <vjagdishp4@gmail.com>
2024-11-20 21:03:03 +0000

    Building and training the model

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
+import numpy as np 
 
 
 # Loading the dataset
@@ -34,3 +35,36 @@ plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color='blue')
 plt.xlabel("Cylinders")
 plt.ylabel("Emission")
 plt.show()
+
+# Splitting data into training and test sets
+#numpy library
+msk = np.random.rand(len(df)) < 0.8 
+train = cdf[msk] 
+test = cdf[~msk]
+
+# Training the model
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.title("Training data")
+plt.show()
+
+from sklearn import linear_model 
+#scikit-learn library:
+regr = linear_model.LinearRegression() 
+
+train_x = np.asanyarray(train[['ENGINESIZE']]) 
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+#scikit-learn library:
+regr.fit(train_x, train_y) 
+
+# The coefficients
+print('Coefficients: ', regr.coef_) 
+print('Intercept: ', regr.intercept_) 
+
+# Plotting the regression line
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color='blue') 
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r') 
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show() 
